f4846df8c70973e96bb9fcf08cd23a6a43efb567
Kristian Klemets <kristian.klemets@anders.fi>
2020-09-22 14:37:15 +0300

    Add django-helusers
    Add tunnistamo integration to the project by initializing
    django-helusers for the project. Create the new user model
    and settings according to the helusers guide.
    
    Refs ASU-239 | Django: Make scaffolding for KeyCloak usage for
    user signing

--- apartment_application_service/urls.py
+++ apartment_application_service/urls.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from helusers.admin_site import admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("pysocial/", include("social_django.urls", namespace="social")),
+    path("helauth/", include("helusers.urls")),
 ]
