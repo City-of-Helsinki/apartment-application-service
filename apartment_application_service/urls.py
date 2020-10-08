741544614740ea23ad5624944af2733bf1e2512e
Kristian Klemets <kristian.klemets@anders.com>
2020-10-08 14:32:33 +0300

    Modify the pysocial url path
    Rename the social_django url path `"pysocial/"` -> `""`. This makes
    it so that the redirect url for tunnistamo will be
    `/complete/tunnistamo/` instead of `/pysocial/complete/tunnistamo/`.
    
    Refs ASU-285

--- apartment_application_service/urls.py
+++ apartment_application_service/urls.py
@@ -3,6 +3,6 @@ from helusers.admin_site import admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("pysocial/", include("social_django.urls", namespace="social")),
+    path("", include("social_django.urls", namespace="social")),
     path("helauth/", include("helusers.urls")),
 ]
