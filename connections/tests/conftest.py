cb8608da2098f9baa06ece992f7a1830ec8ad08d
Tomi Järvi <tomi@jarvi.pro>
2021-03-23 11:57:24 +0000

    Add test conf to connections app

--- connections/tests/conftest.py
+++ connections/tests/conftest.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from elasticsearch.helpers.test import get_test_client, SkipTest
 from elasticsearch_dsl.connections import add_connection
 from pytest import fixture, skip
+
+
+@fixture(autouse=True)
+def use_oikotie_vendor_id():
+    settings.OIKOTIE_VENDOR_ID = "A1234"
 
 
 @fixture(scope="session")
