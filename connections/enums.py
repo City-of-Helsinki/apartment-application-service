653770f4bdbbd89eb5e525c01c8db5861a76a3bf
Evgenia Lyjina <evgenia.lyjina@anders.com>
2021-05-03 07:18:05 +0000

    ASU-702 Add enums for state of sale

--- connections/enums.py
+++ connections/enums.py
@@ -8,3 +8,18 @@ class Currency(Enum):
 class Unit(Enum):
     EUR_KK = "EUR/kk"
     M2 = "m2"
+
+
+class ApartmentStateOfSale(str, Enum):
+    FOR_SALE = "FOR_SALE"
+    OPEN_FOR_APPLICATIONS = "OPEN_FOR_APPLICATIONS"
+    FREE_FOR_RESERVATIONS = "FREE_FOR_RESERVATIONS"
+    RESERVED = "RESERVED"
+    SOLD = "SOLD"
+
+
+class ProjectStateOfSale(str, Enum):
+    PRE_MARKETING = "PRE_MARKETING"
+    FOR_SALE = "FOR_SALE"
+    PROCESSING = "PROCESSING"
+    READY = "READY"
