695cc9293999631cbe20a3654c90d65559a568ab
Tomi Järvi <tomi@jarvi.pro>
2022-01-14 09:58:12 +0000

    fix: validate applicant ssn suffix
    Fix the SSN validation on sales application endpoint that it uses given
    profile instead uses requester's profile. The salesperson can create
    applications on behalf of customers.

--- application_form/api/sales/serializers.py
+++ application_form/api/sales/serializers.py
@@ -1,9 +1,12 @@
 import logging
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework.fields import UUIDField
 
+from application_form import error_codes
 from application_form.api.serializers import ApplicationSerializer
 from application_form.services.application import create_application
+from application_form.validators import SSNSuffixValidator
 from users.models import Profile
 
 _logger = logging.getLogger(__name__)
@@ -33,3 +36,20 @@ class SalesApplicationSerializer(ApplicationSerializer):
 
     def create(self, validated_data):
         return create_application(validated_data)
+
+    def validate_ssn_suffix(self, value):
+        profile_uuid = self.context["request"].data["profile"]
+        applicant_profile = Profile.objects.get(id=profile_uuid)
+        date_of_birth = applicant_profile.date_of_birth
+        validator = SSNSuffixValidator(date_of_birth)
+        try:
+            validator(value)
+        except ValidationError as e:
+            message = f"""Invalid SSN suffix for the primary applicant was
+            received: {e.args[0]}"""
+            _logger.warning(message)
+            raise ValidationError(
+                detail=message,
+                code=error_codes.E1000_SSN_SUFFIX_IS_NOT_VALID,
+            )
+        return value
