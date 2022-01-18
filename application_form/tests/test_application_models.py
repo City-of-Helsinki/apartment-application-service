71296a9f972066e917eb71466ea3d759bcfa6700
Tomi Järvi <tomi@jarvi.pro>
2022-01-18 07:44:20 +0000

    test: remove unnecessary apartment test

--- application_form/tests/test_application_models.py
+++ application_form/tests/test_application_models.py
@@ -1,15 +1,8 @@
 import pytest
 import uuid
 
-from apartment.models import Apartment
-from apartment.tests.factories import ApartmentFactory
-from application_form.models.application import (
-    Applicant,
-    Application,
-    ApplicationApartment,
-)
+from application_form.models.application import Applicant, Application
 from application_form.tests.factories import (
-    ApplicationApartmentFactory,
     ApplicationFactory,
     ApplicationWithApplicantsFactory,
 )
@@ -37,25 +30,3 @@ def test_application_with_applicants_model():
 
     assert Application.objects.first().external_uuid == application_uuid
     assert Applicant.objects.count() == applicants_count
-
-
-@pytest.mark.django_db
-def test_application_has_apartments_model():
-    """Test application_has_apartments model"""
-    application = ApplicationWithApplicantsFactory.create()
-    apartments = ApartmentFactory.create_batch_with_project(5)
-    ApplicationApartmentFactory.create_application_with_apartments(
-        application=application, apartments=apartments
-    )
-
-    # get the set of applications where all apartments belong, should be one
-    apartments_application = set(
-        [apartment.applications.first() for apartment in Apartment.objects.all()]
-    )
-
-    assert len(apartments_application) == 1
-    assert list(apartments_application)[0].id == application.id
-    # check that firs occurance of ApartmenApplication object application's id is same
-    # as firstly created application
-    assert ApplicationApartment.objects.first().application.id == application.id
-    assert list(Application.objects.first().apartments.all()) == apartments
