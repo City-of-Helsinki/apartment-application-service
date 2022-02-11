733c406e94d4f820eff8d5e28ba06a58e37b7d3a
Tuomas Haapala <tuomas.haapala@anders.fi>
2022-02-11 10:57:48 +0200

    Add primary and secondary profile to Customer model

--- customer/tests/factories.py
+++ customer/tests/factories.py
@@ -1,9 +1,10 @@
 """
 Factory classes for test purposes.
 """
-from factory import django, Faker, post_generation
+from factory import django, Faker, SubFactory
 
 from customer.models import Customer
+from users.tests.factories import ProfileFactory
 
 
 class CustomerFactory(django.DjangoModelFactory):
@@ -17,12 +18,4 @@ class CustomerFactory(django.DjangoModelFactory):
     # Others
     additional_information = Faker("paragraph", nb_sentences=5)
     last_contact_date = Faker("date")
-
-    @post_generation
-    def profiles(self, create, extracted, **kwargs):
-        if not create:
-            return
-
-        if extracted:
-            for profile in extracted:
-                self.profiles.add(profile)
+    primary_profile = SubFactory(ProfileFactory)
