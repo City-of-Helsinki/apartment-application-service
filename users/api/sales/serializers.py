edec936df801636e1712fab78c4199d6e9a11296
Tomi Järvi <tomi@jarvi.pro>
2022-02-18 13:54:16 +0000

    feat: add contact information to customer detail api endpoint

--- users/api/sales/serializers.py
+++ users/api/sales/serializers.py
@@ -3,4 +3,10 @@ from users.api.serializers import ProfileSerializerBase
 
 class ProfileSerializer(ProfileSerializerBase):
     class Meta(ProfileSerializerBase.Meta):
-        fields = ProfileSerializerBase.Meta.fields + ("national_identification_number",)
+        fields = ProfileSerializerBase.Meta.fields + (
+            "national_identification_number",
+            "street_address",
+            "postal_code",
+            "city",
+            "contact_language",
+        )
