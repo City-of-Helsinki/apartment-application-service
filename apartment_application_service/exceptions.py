14c92239518c1732a8a50661fd1e46e74457afab
Tuomas Haapala <tuomas.haapala@anders.fi>
2022-05-12 16:32:16 +0300

    Convert unhandled Django ValidationErrors to DRF ones
    This is done so that more business logic can be centered to "services"
    from serializers.

--- apartment_application_service/exceptions.py
+++ apartment_application_service/exceptions.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import ValidationError
 from rest_framework import exceptions
+from rest_framework.exceptions import ValidationError as DRFValidationError
+from rest_framework.fields import get_error_detail
 from rest_framework.views import exception_handler
 
 
@@ -6,6 +9,13 @@ def drf_exception_handler(exc, context):
     """
     Returns full details from APIException.
     """
+
+    # convert Django ValidationError to DRF ValidationError
+    if isinstance(exc, ValidationError):
+        return exception_handler(
+            DRFValidationError(detail=get_error_detail(exc)), context
+        )
+
     response = exception_handler(exc, context)
 
     if response is not None and isinstance(exc, exceptions.APIException):
