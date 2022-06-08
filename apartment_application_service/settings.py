5baf5d6c5fb31dc583c26684ed465d25e2d362f5
Tuomas Haapala <tuomas.haapala@anders.fi>
2022-06-08 17:10:23 +0300

    Allow only salesperson group users use Sales API

--- apartment_application_service/settings.py
+++ apartment_application_service/settings.py
@@ -252,7 +252,7 @@ REST_FRAMEWORK = {
         "apartment_application_service.oidc.TunnistamoFixedApiTokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": ("users.permissions.IsSalesperson",),
     "DEFAULT_SCHEMA_CLASS": "apartment_application_service.openapi.AutoSchema",
     "EXCEPTION_HANDLER": "apartment_application_service.exceptions.drf_exception_handler",  # noqa: E501
 }
