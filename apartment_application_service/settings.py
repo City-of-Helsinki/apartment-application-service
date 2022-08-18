a482c6668c86e18c8ab4f7577dbbdc1b734f9341
Khin Loc <khin.loc@anders.fi>
2022-08-18 18:03:03 +0300

    Send email notificationt to Talpa when new installments batch sent to SAP, fix old test

--- apartment_application_service/settings.py
+++ apartment_application_service/settings.py
@@ -31,7 +31,7 @@ env = environ.Env(
         "@localhost/apartment-application",
     ),
     CACHE_URL=(str, "locmemcache://"),
-    DEFAULT_FROM_EMAIL=(str, ""),
+    DEFAULT_FROM_EMAIL=(str, "asuntomyynti@hel.fi"),
     MAIL_MAILGUN_KEY=(str, ""),
     MAIL_MAILGUN_DOMAIN=(str, ""),
     MAIL_MAILGUN_API=(str, ""),
@@ -84,6 +84,8 @@ env = environ.Env(
     ),
     METADATA_HITAS_PROCESS_NUMBER=(str, "10 07 05 00"),
     METADATA_HASO_PROCESS_NUMBER=(str, "10 07 04 01"),
+    TALPA_EMAIL=(str, ""),
+    TALPA_EMAIL_REPLY_TO=(str, "asuntomyynti@hel.fi"),
 )
 if os.path.exists(env_file):
     env.read_env(env_file)
@@ -361,6 +363,9 @@ PRIVATE_PGP_KEY = env.str("PRIVATE_PGP_KEY", multiline=True)
 METADATA_HANDLER_INFORMATION = env.str("METADATA_HANDLER_INFORMATION")
 METADATA_HITAS_PROCESS_NUMBER = env.str("METADATA_HITAS_PROCESS_NUMBER")
 METADATA_HASO_PROCESS_NUMBER = env.str("METADATA_HASO_PROCESS_NUMBER")
+
+TALPA_EMAIL = env.str("TALPA_EMAIL")
+TALPA_EMAIL_REPLY_TO = env.str("TALPA_EMAIL_REPLY_TO")
 
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
--- invoicing/services.py
+++ invoicing/services.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from django.conf import settings
+from django.core.mail import EmailMessage
 from django.utils import timezone
+from django.utils.timezone import localtime
 from io import BytesIO
 from logging import getLogger
 
@@ -12,31 +15,56 @@ from invoicing.sap.xml import generate_installments_xml
 logger = getLogger(__name__)
 
 
+TALPA_EMAIL_SUBJECT_TEMPLATE = "{sender_id} aineisto"
+TALPA_EMAIL_CONTENT_TEMPLATE = (
+    "{sender_id} myyntireskontra-aineistoa lähetetty {count} kpl {date_time}"
+)
+
+
 def send_needed_installments_to_sap() -> int:
     installments = ApartmentInstallment.objects.sending_to_sap_needed()
     logger.debug(f"Installment IDs: {[i.pk for i in installments]}")
     num_of_installments = installments.count()
+    timestamp = timezone.now()
     if num_of_installments:
         xml = generate_installments_xml(installments)
         logger.debug(f"Generated XML:\n{xml.decode('utf-8')}")
-        send_xml_to_sap(xml)
+        send_xml_to_sap(xml, timestamp)
         installments.set_sent_to_sap_at()
         for installment in installments:
             audit_logging.log(None, Operation.UPDATE, installment)
-    return num_of_installments
+    return num_of_installments, timestamp
 
 
-def generate_sap_xml_filename() -> str:
+def send_email_notification_to_talpa(count: int, timestamp: datetime):
+    email = EmailMessage(
+        subject=TALPA_EMAIL_SUBJECT_TEMPLATE.format(
+            sender_id=settings.SAP["SENDER_ID"]
+        ),
+        body=TALPA_EMAIL_CONTENT_TEMPLATE.format(
+            sender_id=settings.SAP["SENDER_ID"],
+            count=count,
+            date_time=localtime(timestamp).strftime("%d.%m.%Y %H:%M:%S"),
+        ),
+        to=[settings.TALPA_EMAIL],
+        reply_to=[settings.TALPA_EMAIL_REPLY_TO],
+    )
+    return email.send()
+
+
+def generate_sap_xml_filename(timestamp: datetime) -> str:
     return (
         settings.SAP_SFTP_FILENAME_PREFIX
-        + f"{timezone.now().strftime('%Y%m%d%H%M%S')}"
+        + f"{timestamp.strftime('%Y%m%d%H%M%S')}"
         + ".xml"
     )
 
 
-def send_xml_to_sap(xml: bytes, filename: str = None) -> None:
+def send_xml_to_sap(
+    xml: bytes, filename: str = None, timestamp: datetime = None
+) -> None:
     if filename is None:
-        filename = generate_sap_xml_filename()
+        filename = generate_sap_xml_filename(timestamp)
     logger.debug(
         f"Sending XML file {filename} "
         f"to {settings.SAP_SFTP_HOST}:{settings.SAP_SFTP_PORT}"
