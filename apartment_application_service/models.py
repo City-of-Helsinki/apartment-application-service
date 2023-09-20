a3cc69791ea98e8f497c08a2add25ec97f8952ca
Tuomas Haapala <tuomas.haapala@anders.fi>
2023-09-20 22:36:36 +0300

    Refactor common model fields to one model
    Moved common fields from Application, ApartmentReservation and Customer
    models to one abstract base model.

--- apartment_application_service/models.py
+++ apartment_application_service/models.py
@@ -1,9 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from pgcrypto.fields import IntegerPGPPublicKeyField
+
+from apartment_application_service.fields import BooleanPGPPublicKeyField
 
 
 class TimestampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class CommonApplicationData(models.Model):
+    """Common fields used in Application, ApartmentReservation and Customer models."""
+
+    is_right_of_occupancy_housing_changer = BooleanPGPPublicKeyField(
+        _("is right-of-occupancy housing changer"), blank=True, null=True
+    )
+    has_hitas_ownership = BooleanPGPPublicKeyField(
+        _("has HITAS ownership"), blank=True, null=True
+    )
+    right_of_residence = IntegerPGPPublicKeyField(
+        _("right of residence number"), blank=True, null=True
+    )
 
     class Meta:
         abstract = True
