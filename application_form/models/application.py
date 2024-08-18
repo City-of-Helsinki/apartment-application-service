2ae0c9e2ab5e54b9de1f535306b6f3a7eb9b4437
Matti Eiden <matti.eiden@anders.com>
2024-08-18 12:37:40 +0300

    fix: decrypt application
    Includes Applicant, Application and ApplicationApartment.
    
    This one was fairly interesting because there are encrypted fields with
    a default value. django-pgcrypto-fields does not support default values
    (on the db level). See:
     https://github.com/incuna/django-pgcrypto-fields/issues/298
    
    The way django uses `default` has a catch - if the
    field is nullable, django does nothing on the DB side with DEFAULT. If
    the field is NOT nullable, then django will apply the DEFAULT and
    immediately after DROP DEFAULT. For an encrypted field this will not
    work as it will attempt to set the primitive value to the encrypted
    field which has a type of bytea.
    
    One way to encounter the catch is to remove a field that is NOT NULL
    and has a DEFAULT:
    the reverse migration will attempt to create the field with the DEFAULT
    value --> kaboom.
    
    Attempting to ALTER field from NOT NULL + DEFAULT -> NULL will
    also fail. Django attempts to use the previous default in this
    migration.
    
    So the correct way is to go in two steps and first remove the default
    and then allow nulls:
    
    NOT NULL + DEFAULT -> NOT NULL -> NULL

--- application_form/models/application.py
+++ application_form/models/application.py
@@ -4,18 +4,8 @@ from django.core.validators import MinLengthValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from enumfields import EnumField
-from pgcrypto.fields import (
-    CharPGPPublicKeyField,
-    DatePGPPublicKeyField,
-    EmailPGPPublicKeyField,
-    IntegerPGPPublicKeyField,
-)
+from pgcrypto.fields import CharPGPPublicKeyField, DatePGPPublicKeyField
 
-from apartment_application_service.fields import (
-    BooleanPGPPublicKeyField,
-    EnumPGPPublicKeyField,
-    UUIDPGPPublicKeyField,
-)
 from apartment_application_service.models import CommonApplicationData, TimestampedModel
 from application_form.enums import ApplicationArrivalMethod, ApplicationType
 from customer.models import Customer
@@ -23,17 +13,17 @@ from users.models import Profile
 
 
 class Application(TimestampedModel, CommonApplicationData):
-    external_uuid = UUIDPGPPublicKeyField(
-        _("application identifier"), default=uuid4, editable=False
-    )
-    applicants_count = IntegerPGPPublicKeyField(_("applicants count"))
-    type = EnumPGPPublicKeyField(
-        ApplicationType, max_length=15, verbose_name=_("application type")
-    )
-    has_children = BooleanPGPPublicKeyField(_("has children"), default=False)
     customer = models.ForeignKey(
         Customer, verbose_name=_("customer"), on_delete=models.CASCADE
     )
+    external_uuid = models.UUIDField(
+        _("application identifier"), default=uuid4, editable=False
+    )
+
+    applicants_count = models.IntegerField(_("applicants count"))
+    type = EnumField(ApplicationType, max_length=15, verbose_name=_("application type"))
+    has_children = models.BooleanField(_("has children"), default=False)
+
     submitted_late = models.BooleanField("submitted late", default=False)
 
     # Metadata fields
@@ -45,35 +35,33 @@ class Application(TimestampedModel, CommonApplicationData):
         verbose_name=_("method of arrival"),
         default=ApplicationArrivalMethod.ELECTRONICAL_SYSTEM,
     )
-    sender_names = CharPGPPublicKeyField(_("sender names"), max_length=200)
+    sender_names = models.CharField(_("sender names"), max_length=200)
 
     audit_log_id_field = "external_uuid"
 
 
 class Applicant(TimestampedModel):
-    first_name = CharPGPPublicKeyField(_("first name"), max_length=30)
-    last_name = CharPGPPublicKeyField(_("last name"), max_length=150)
-    email = EmailPGPPublicKeyField(_("email"))
-    phone_number = CharPGPPublicKeyField(_("phone number"), max_length=40, null=False)
-    street_address = CharPGPPublicKeyField(_("street address"), max_length=200)
-    city = CharPGPPublicKeyField(_("city"), max_length=50)
-    postal_code = CharPGPPublicKeyField(_("postal code"), max_length=10)
-    age = IntegerPGPPublicKeyField(_("age"))
+    first_name = models.CharField(_("first name"), max_length=30)
+    last_name = models.CharField(_("last name"), max_length=150)
+    email = models.EmailField(_("email"))
+    phone_number = models.CharField(_("phone number"), max_length=40)
+    street_address = models.CharField(_("street address"), max_length=200)
+    city = models.CharField(_("city"), max_length=50)
+    postal_code = models.CharField(_("postal code"), max_length=10)
+    age = models.IntegerField(_("age"))
     date_of_birth = DatePGPPublicKeyField(_("date of birth"))
     ssn_suffix = CharPGPPublicKeyField(
         "personal identity code suffix",
         max_length=5,
         validators=[MinLengthValidator(5)],
     )
-    contact_language = CharPGPPublicKeyField(
+    contact_language = models.CharField(
         _("contact language"),
         max_length=2,
         choices=Profile.CONTACT_LANGUAGE_CHOICES,
         null=True,
     )
-    is_primary_applicant = BooleanPGPPublicKeyField(
-        _("is primary applicant"), default=False
-    )
+    is_primary_applicant = models.BooleanField(_("is primary applicant"), default=False)
 
     application = models.ForeignKey(
         Application, on_delete=models.CASCADE, related_name="applicants"
@@ -85,7 +73,7 @@ class ApplicationApartment(models.Model):
         Application, on_delete=models.CASCADE, related_name="application_apartments"
     )
     apartment_uuid = models.UUIDField(verbose_name=_("apartment uuid"))
-    priority_number = IntegerPGPPublicKeyField(_("priority number"))
+    priority_number = models.IntegerField(_("priority number"))
 
     class Meta:
         unique_together = [("application", "priority_number")]
--- application_form/tests/test_migrations.py
+++ application_form/tests/test_migrations.py
@@ -1,3 +1,4 @@
+import random
 import uuid
 from datetime import date
 
@@ -5,10 +6,15 @@ import pgcrypto.fields
 import pytest
 from django.db import models
 from django.db.models import Model
+from enumfields import EnumField
 from faker import Faker
 from pgcrypto.fields import IntegerPGPPublicKeyField
 
-from apartment_application_service.fields import BooleanPGPPublicKeyField
+from apartment_application_service.fields import (
+    BooleanPGPPublicKeyField,
+    EnumPGPPublicKeyField,
+    UUIDPGPPublicKeyField,
+)
 from application_form.enums import ApplicationType
 
 
@@ -233,3 +239,261 @@ def test_0072_decrypt_common_application_data(migrator):  # noqa: C901
 
     for k, v in apartment_reservation_2_test_data.items():
         assert getattr(apartment_reservation_2, k) == v
+
+
+def test_0073_decrypt_application(migrator):  # noqa: C901
+    faker = Faker()
+    old_state = migrator.apply_initial_migration(
+        ("application_form", "0072_decrypt_common_application_data")
+    )
+    OldProfile: Model = old_state.apps.get_model("users", "Profile")
+    OldCustomer: Model = old_state.apps.get_model("customer", "Customer")
+    OldApplication: Model = old_state.apps.get_model("application_form", "Application")
+    OldApplicant: Model = old_state.apps.get_model("application_form", "Applicant")
+    OldApplicationApartment: Model = old_state.apps.get_model(
+        "application_form", "ApplicationApartment"
+    )
+
+    profile_1 = OldProfile.objects.create(date_of_birth=date.today())
+    profile_2 = OldProfile.objects.create(date_of_birth=date.today())
+
+    customer_1 = OldCustomer.objects.create(
+        primary_profile=profile_1,
+    )
+    customer_2 = OldCustomer.objects.create(
+        primary_profile=profile_2,
+    )
+
+    application_encrypted_field_map = {
+        "applicants_count": IntegerPGPPublicKeyField,
+        "external_uuid": UUIDPGPPublicKeyField,
+        "has_children": BooleanPGPPublicKeyField,
+        "sender_names": pgcrypto.fields.CharPGPPublicKeyField,
+        "type": EnumPGPPublicKeyField,
+    }
+
+    application_decrypted_field_map = {
+        "applicants_count": models.IntegerField,
+        "external_uuid": models.UUIDField,
+        "has_children": models.BooleanField,
+        "sender_names": models.CharField,
+        "type": EnumField,
+    }
+
+    applicant_encrypted_field_map = {
+        "age": IntegerPGPPublicKeyField,
+        "city": pgcrypto.fields.CharPGPPublicKeyField,
+        "contact_language": pgcrypto.fields.CharPGPPublicKeyField,
+        "email": pgcrypto.fields.EmailPGPPublicKeyField,
+        "first_name": pgcrypto.fields.CharPGPPublicKeyField,
+        "is_primary_applicant": BooleanPGPPublicKeyField,
+        "last_name": pgcrypto.fields.CharPGPPublicKeyField,
+        "phone_number": pgcrypto.fields.CharPGPPublicKeyField,
+        "postal_code": pgcrypto.fields.CharPGPPublicKeyField,
+        "street_address": pgcrypto.fields.CharPGPPublicKeyField,
+        "date_of_birth": pgcrypto.fields.DatePGPPublicKeyField,  # bonus check
+        "ssn_suffix": pgcrypto.fields.CharPGPPublicKeyField,  # bonus check
+    }
+
+    applicant_decrypted_field_map = {
+        "age": models.IntegerField,
+        "city": models.CharField,
+        "contact_language": models.CharField,
+        "email": models.EmailField,
+        "first_name": models.CharField,
+        "is_primary_applicant": models.BooleanField,
+        "last_name": models.CharField,
+        "phone_number": models.CharField,
+        "postal_code": models.CharField,
+        "street_address": models.CharField,
+        "date_of_birth": pgcrypto.fields.DatePGPPublicKeyField,  # bonus check
+        "ssn_suffix": pgcrypto.fields.CharPGPPublicKeyField,  # bonus check
+    }
+
+    application_apt_encrypted_field_map = {
+        "priority_number": IntegerPGPPublicKeyField,
+    }
+
+    application_apt_decrypted_field_map = {
+        "priority_number": models.IntegerField,
+    }
+
+    def get_application_test_data():
+        return {
+            "applicants_count": 1,
+            "external_uuid": uuid.uuid4(),
+            "has_children": faker.boolean(),
+            "sender_names": faker.first_name(),
+            "type": random.choice(
+                [
+                    ApplicationType.HITAS,
+                    ApplicationType.HASO,
+                    ApplicationType.PUOLIHITAS,
+                ]
+            ),
+        }
+
+    def get_applicant_test_data():
+        return {
+            "age": faker.random_number(digits=2),
+            "city": faker.city(),
+            "contact_language": faker.country_code(),
+            "email": faker.email(),
+            "first_name": faker.first_name(),
+            "is_primary_applicant": faker.boolean(),
+            "last_name": faker.last_name(),
+            "phone_number": faker.phone_number(),
+            "postal_code": str(faker.random_number(digits=5, fix_len=True)),
+            "street_address": faker.address(),
+        }
+
+    def get_application_apt_test_data():
+        return {"priority_number": faker.random_number()}
+
+    application_1_test_data = get_application_test_data()
+    application_2_test_data = get_application_test_data()
+
+    application_1 = OldApplication.objects.create(
+        customer=customer_1, **application_1_test_data
+    )
+    application_2 = OldApplication.objects.create(
+        customer=customer_2, **application_2_test_data
+    )
+
+    applicant_1_test_data = get_applicant_test_data()
+    applicant_2_test_data = get_applicant_test_data()
+
+    applicant_1 = OldApplicant.objects.create(
+        application=application_1, date_of_birth=date.today(), **applicant_1_test_data
+    )
+    applicant_2 = OldApplicant.objects.create(
+        application=application_2, date_of_birth=date.today(), **applicant_2_test_data
+    )
+
+    application_apt_1_test_data = get_application_apt_test_data()
+    application_apt_2_test_data = get_application_apt_test_data()
+
+    application_apt_1 = OldApplicationApartment.objects.create(
+        application=application_1,
+        apartment_uuid=uuid.uuid4(),
+        **application_apt_1_test_data,
+    )
+    application_apt_2 = OldApplicationApartment.objects.create(
+        application=application_2,
+        apartment_uuid=uuid.uuid4(),
+        **application_apt_2_test_data,
+    )
+
+    new_state = migrator.apply_tested_migration(
+        ("application_form", "0073_decrypt_application")
+    )
+
+    NewApplication: Model = new_state.apps.get_model("application_form", "Application")
+    NewApplicant: Model = new_state.apps.get_model("application_form", "Applicant")
+    NewApplicationApartment: Model = new_state.apps.get_model(
+        "application_form", "ApplicationApartment"
+    )
+
+    def assert_field_type(model, field_name, expected_type):
+        assert (field_name, type(model._meta.get_field(field_name))) == (
+            field_name,
+            expected_type,
+        )
+
+    for k, v in application_decrypted_field_map.items():
+        assert_field_type(NewApplication, k, v)
+
+    for k, v in applicant_decrypted_field_map.items():
+        assert_field_type(NewApplicant, k, v)
+
+    for k, v in application_apt_decrypted_field_map.items():
+        assert_field_type(NewApplicationApartment, k, v)
+
+    assert NewApplication.objects.all().count() == 2
+    application_1 = NewApplication.objects.get(pk=application_1.pk)
+    application_2 = NewApplication.objects.get(pk=application_2.pk)
+
+    for k, v in application_1_test_data.items():
+        assert getattr(application_1, k) == v
+
+    for k, v in application_2_test_data.items():
+        assert getattr(application_2, k) == v
+
+    assert NewApplicant.objects.all().count() == 2
+    applicant_1 = NewApplicant.objects.get(pk=applicant_1.pk)
+    applicant_2 = NewApplicant.objects.get(pk=applicant_2.pk)
+
+    for k, v in applicant_1_test_data.items():
+        assert getattr(applicant_1, k) == v
+
+    for k, v in applicant_2_test_data.items():
+        assert getattr(applicant_2, k) == v
+
+    assert NewApplicationApartment.objects.all().count() == 2
+    application_apt_1 = NewApplicationApartment.objects.get(pk=application_apt_1.pk)
+    application_apt_2 = NewApplicationApartment.objects.get(pk=application_apt_2.pk)
+
+    for k, v in application_apt_1_test_data.items():
+        assert getattr(application_apt_1, k) == v
+
+    for k, v in application_apt_2_test_data.items():
+        assert getattr(application_apt_2, k) == v
+
+    revert_state = migrator.apply_tested_migration(
+        ("application_form", "0072_decrypt_common_application_data")
+    )
+
+    RevertApplication: Model = revert_state.apps.get_model(
+        "application_form", "Application"
+    )
+    RevertApplicant: Model = revert_state.apps.get_model(
+        "application_form", "Applicant"
+    )
+    RevertApplicationApartment: Model = revert_state.apps.get_model(
+        "application_form", "ApplicationApartment"
+    )
+
+    def assert_field_type(model, field_name, expected_type):
+        assert (field_name, type(model._meta.get_field(field_name))) == (
+            field_name,
+            expected_type,
+        )
+
+    for k, v in application_encrypted_field_map.items():
+        assert_field_type(RevertApplication, k, v)
+
+    for k, v in applicant_encrypted_field_map.items():
+        assert_field_type(RevertApplicant, k, v)
+
+    for k, v in application_apt_encrypted_field_map.items():
+        assert_field_type(RevertApplicationApartment, k, v)
+
+    assert RevertApplication.objects.all().count() == 2
+    application_1 = RevertApplication.objects.get(pk=application_1.pk)
+    application_2 = RevertApplication.objects.get(pk=application_2.pk)
+
+    for k, v in application_1_test_data.items():
+        assert getattr(application_1, k) == v
+
+    for k, v in application_2_test_data.items():
+        assert getattr(application_2, k) == v
+
+    assert RevertApplicant.objects.all().count() == 2
+    applicant_1 = RevertApplicant.objects.get(pk=applicant_1.pk)
+    applicant_2 = RevertApplicant.objects.get(pk=applicant_2.pk)
+
+    for k, v in applicant_1_test_data.items():
+        assert getattr(applicant_1, k) == v
+
+    for k, v in applicant_2_test_data.items():
+        assert getattr(applicant_2, k) == v
+
+    assert RevertApplicationApartment.objects.all().count() == 2
+    application_apt_1 = RevertApplicationApartment.objects.get(pk=application_apt_1.pk)
+    application_apt_2 = RevertApplicationApartment.objects.get(pk=application_apt_2.pk)
+
+    for k, v in application_apt_1_test_data.items():
+        assert getattr(application_apt_1, k) == v
+
+    for k, v in application_apt_2_test_data.items():
+        assert getattr(application_apt_2, k) == v
