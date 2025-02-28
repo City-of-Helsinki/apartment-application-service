be43df486138dfef9c3c9a04496f5643196d62c9
leevi-identio <leevi@identio.fi>
2025-02-28 14:56:49 +0200

    add debug logging

--- connections/oikotie/services.py
+++ connections/oikotie/services.py
@@ -68,6 +68,7 @@ def create_xml_apartment_file(apartments: list) -> Optional[str]:
         ap_file = create_apartments(apartments, path)
         _logger.info(f"Created XML file for apartments in location {path}/{ap_file}")
 
+        _logger.debug("os.listdir(os.path.join(path, ap_file)) %s", os.listdir(os.path.join(path, ap_file)))
         valid = validate_against_schema(
             settings.OIKOTIE_APARTMENTS_BATCH_SCHEMA, os.path.join(path, ap_file)
         )
