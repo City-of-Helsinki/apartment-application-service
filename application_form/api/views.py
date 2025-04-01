4e0d96cd456ec07e80b12ee0b6b958d724581be3
andreybaklanov <andrey.baklanov@twoday.com>
2025-04-01 12:51:36 +0300

    ASU-1733 remove unused import

--- application_form/api/views.py
+++ application_form/api/views.py
@@ -1,9 +1,4 @@
 from rest_framework import permissions, status
-from rest_framework.decorators import (
-    api_view,
-    authentication_classes,
-    permission_classes,
-)
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
