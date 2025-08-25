d2e2c9a71a9a07eb5b1c0502546f57f79530ed20
andreybaklanov <andrey.baklanov@twoday.com>
2025-08-25 09:18:41 +0300

    fix tests

--- application_form/tests/test_export_service.py
+++ application_form/tests/test_export_service.py
@@ -206,6 +206,7 @@ def _validate_mailing_list_csv(
             secondary_profile.city or "",
             secondary_profile.national_identification_number or "",
             "X" if bool(reservation.has_children) else "",
+            "Kyllä" if bool(reservation.has_hitas_ownership) else "Ei",
             apartment.project_street_address or "",
             apartment.project_postal_code or "",
             apartment.project_city or "",
@@ -260,12 +261,12 @@ def test_sorting_function(reservations):
     "export_type,project_ownership_type,expected_column_count",
     [
         (ApartmentReservationState.SOLD.value, OwnershipType.HASO, 28),
-        (ApartmentReservationState.SOLD.value, OwnershipType.HITAS, 22),
-        (ApartmentReservationState.RESERVED.value, OwnershipType.HASO, 22),
+        (ApartmentReservationState.SOLD.value, OwnershipType.HITAS, 23),
+        (ApartmentReservationState.RESERVED.value, OwnershipType.HASO, 23),
         (
             ApplicantMailingListExportService.export_first_in_queue,
             OwnershipType.HASO,
-            22,
+            23,
         ),
     ],
 )
