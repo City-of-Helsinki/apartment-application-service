6d9c6478c86677c8d1d8ca5db2f8183761c14a6c
leevi-identio <leevi@identio.fi>
2025-09-02 12:32:06 +0300

    Add simple test cases for late submit on `/v1/applications/` endpoint
    - refactor test cases for `/v1/sales/applications/` endpoints
    
    - refactor, move late-submit logic to parent serializer of `SalesApplicationSerializer`

--- application_form/tests/utils.py
+++ application_form/tests/utils.py
@@ -3,37 +3,41 @@ import random
 import uuid
 from datetime import date
 from typing import List, Tuple
+
 from django.utils import timezone
-from django.db.models.fields import settings
+
 from apartment.elastic.documents import ApartmentDocument
 from apartment.elastic.queries import apartment_query
-from elasticsearch_dsl import Search
-
 from connections.enums import ApartmentStateOfSale
 from users.models import Profile
 
 _logger = logging.getLogger(__name__)
 
+
 def get_for_sale_elastic_apartments():
     return apartment_query(
-        _language="fi",
-        apartment_state_of_sale=ApartmentStateOfSale.FOR_SALE
+        _language="fi", apartment_state_of_sale=ApartmentStateOfSale.FOR_SALE
     )
+
 
 def get_elastic_apartments_with_application_time_left():
     return [
-            apt for apt in get_for_sale_elastic_apartments()
-            if timezone.now() <= apt.project_application_end_time
+        apt
+        for apt in get_for_sale_elastic_apartments()
+        if timezone.now() <= apt.project_application_end_time
     ]
 
-def get_elastic_apartments_uuids(apartments: List[ApartmentDocument]) -> Tuple[uuid.UUID, List[uuid.UUID]]:
+
+def get_elastic_apartments_uuids(
+    apartments: List[ApartmentDocument],
+) -> Tuple[uuid.UUID, List[uuid.UUID]]:
     """Extract project_uuid and uuids from `ApartmentDocument` objects.
 
     Args:
         apartments (List[ApartmentDocument]): List of `ApartmentDocument` objs
 
     Returns:
-        Tuple[uuid.UUID, List[uuid.UUID]]: `project_uuid` common to all the 
+        Tuple[uuid.UUID, List[uuid.UUID]]: `project_uuid` common to all the
         ApartmentDocuments and list of uuids for each ApartmentDocument
     """
     uuids = []
