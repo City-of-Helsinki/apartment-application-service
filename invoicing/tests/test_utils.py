12c5f7624e67a8fa55eb850e8463e0e6b000e318
andreybaklanov <andrey.baklanov@twoday.com>
2025-09-22 14:45:41 +0300

    Cherry-pick ASU-1738 to release
    ASU-1738 change postingDate logic
    
    ASU-1738 postingDate tests
    
    ASU-1738 improve tests for postingDate
    
    ASU-1738 fix lint error

--- invoicing/tests/test_utils.py
+++ invoicing/tests/test_utils.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 import pytest
 
+from invoicing.sap.send.xml_utils import get_posting_date
 from invoicing.utils import generate_reference_number
 
 
@@ -13,3 +16,21 @@ from invoicing.utils import generate_reference_number
 )
 def test_generate_reference_number(identifier, expected):
     assert generate_reference_number(identifier) == expected
+
+
+class FakeDate(date):
+    @classmethod
+    def today(cls):
+        return date(2025, 9, 1)
+
+
+@pytest.mark.parametrize(
+    "due_date, expected",
+    [
+        (date(2025, 9, 23), "20250901"),
+        (date(2025, 10, 10), "20250910"),
+    ],
+)
+def test_get_posting_date(monkeypatch, due_date, expected):
+    monkeypatch.setattr("invoicing.sap.send.xml_utils.date", FakeDate)
+    assert get_posting_date(due_date) == expected
