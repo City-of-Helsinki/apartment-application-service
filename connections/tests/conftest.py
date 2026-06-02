6102e38458a33e0c42cc419233c71ab4cdd43c91
leevi-identio <leevi@identio.fi>
2026-06-02 13:33:53 +0300

    Fix lint err

--- connections/tests/conftest.py
+++ connections/tests/conftest.py
@@ -8,7 +8,6 @@ from rest_framework.test import APIClient
 import connections
 from apartment.tests.factories import (
     add_to_store,
-    APARTMENT_STORE,
     clear_apartment_store,
 )
 from apartment.tests.utils import TestDrupalSearchClient
@@ -185,67 +184,3 @@ def invalid_data_elastic_apartments_for_sale(elastic_apartments):
     add_to_store(apartments)
 
     yield apartments
-
-
-@fixture(autouse=True)
-def mock_connections_apartment_search(monkeypatch):
-    from connections.enums import ApartmentStateOfSale
-    from connections.etuovi.etuovi_mapper import map_apartment_to_item
-    from connections.oikotie.oikotie_mapper import (
-        map_oikotie_apartment,
-        map_oikotie_housing_company,
-    )
-    from connections.utils import build_project_floor_max_by_uuid, map_document
-
-    def _etuovi_source():
-        return [
-            apt
-            for apt in APARTMENT_STORE
-            if apt._language == "fi"
-            and apt.apartment_state_of_sale == ApartmentStateOfSale.FOR_SALE
-            and apt.publish_on_etuovi is True
-        ]
-
-    def _oikotie_source():
-        return [
-            apt
-            for apt in APARTMENT_STORE
-            if apt._language == "fi"
-            and apt.apartment_state_of_sale == ApartmentStateOfSale.FOR_SALE
-            and apt.publish_on_oikotie is True
-        ]
-
-    def _etuovi_fetch_apartments_for_sale(verbose=False):
-        source = _etuovi_source()
-        project_floor_max_lookup = build_project_floor_max_by_uuid(source)
-
-        def map_item(apartment):
-            return map_apartment_to_item(
-                apartment, project_floor_max_lookup=project_floor_max_lookup
-            )
-
-        items = []
-        for hit in source:
-            apartment = map_document(hit, map_item)
-            if apartment:
-                items.append(apartment)
-        return items
-
-    def _oikotie_fetch_apartments_for_sale():
-        source = _oikotie_source()
-        project_floor_max_lookup = build_project_floor_max_by_uuid(source)
-
-        def map_apartment(apartment):
-            return map_oikotie_apartment(
-                apartment, project_floor_max_lookup=project_floor_max_lookup
-            )
-
-        apartments = []
-        housing_companies = []
-        for hit in source:
-            apartment = map_document(hit, map_apartment)
-            housing = map_document(hit, map_oikotie_housing_company)
-            if apartment and housing:
-                apartments.append(apartment)
-                housing_companies.append(housing)
-        return apartments, housing_companies
